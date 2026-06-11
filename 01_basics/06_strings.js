64ab058fbbfc7a281e8b9c39aae75871df92ec34
Devanshu Rana <devanshurana84@gmail.com>
2026-06-11 16:31:33 +0000

    string class is completed

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -14,19 +14,25 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(7));
 console.log(gameName.indexOf('h'));
 
- const str = gameName.substring(0,1);
- console.log(str);
+const str = gameName.substring(0, 1);
+console.log(str);
 
- const str2 = gameName.slice(-8,4);
- console.log(str2);
+const str2 = gameName.slice(-8, 4);
+console.log(str2);
 
- const trimm = "       Devanshu      ";
- console.log(trimm.trim());
+const trimm = "       Devanshu      ";
+console.log(trimm.trim());
 
- const url = "https://dev.com/dev%20rana";
-  const url2 = url.trim();
- console.log(url2.replace('%20','-'));
+const url = "https://dev.com/dev%20rana";
+const url2 = url.trim();
+console.log(url2.replace('%20', '-'));
 
- console.log(url.includes('dev'));
+console.log(url.includes('dev'));
 
- console.log(gameName.split('-'));
+console.log(gameName.split('-'));
+
+
+
+let name = "Devanshu Rana";
+let age = 18;
+console.log(`My name is  ${name} and My age is ${age}`);
